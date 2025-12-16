2436534a568438089c6f3ce698b939848c364854
Jacob Scott Farmer <xboxteejaymcfarmer@gmail.com>
2025-12-16 01:45:18 -0700

    Update player_status.gd

--- scripts/player/player_status.gd
+++ scripts/player/player_status.gd
@@ -1,41 +1,36 @@
+# File: scripts/player/PlayerStatus.gd
 extends Node
-class_name PlayerStatus
 
 @export var vitality_system: PlayerVitalitySystem
+@export var pools: PlayerPools
+@export var race: RaceDefinition
+@export var factions: FactionSystem
+@export var infection_model: CellInfectionModel
 
 func _ready() -> void:
-    if vitality_system == null:
-        vitality_system = PlayerVitalitySystem.new()
+    race.apply_to_vitality_system(vitality_system)
     vitality_system.recalc_maxima()
-    DebugLog.log("PlayerStatus", "ATTRIBUTES_INIT", {
+    pools.vitality_system = vitality_system
+    pools.recalc_from_vitality()
+    DebugLog.log("PlayerStatus", "INIT", {
+        "race": race.display_name,
         "vitality": vitality_system.vitality,
-        "instinct": vitality_system.instinct,
-        "tenacity": vitality_system.tenacity,
-        "agility": vitality_system.agility,
-        "logic": vitality_system.logic,
-        "influence": vitality_system.influence,
-        "temper": vitality_system.temper,
-        "yield": vitality_system.yield,
-        "constitution": vitality_system.constitution,
-        "dexterity": vitality_system.dexterity,
-        "intelligence": vitality_system.intelligence,
-        "luck": vitality_system.luck,
-        "speed": vitality_system.speed,
-        "strength": vitality_system.strength,
-        "blood": vitality_system.blood,
-        "oxygen": vitality_system.oxygen,
-        "stamina": vitality_system.stamina,
-        "wellness": vitality_system.wellness,
-        "body_temp": vitality_system.body_temperature
+        "oxygen_max": vitality_system.oxygen_max
     })
 
-func tick_environment(delta: float, env_cold: float, env_stress: float) -> void:
+func _physics_process(delta: float) -> void:
+    var env_cold := GameState.current_region_cold
+    var env_stress := GameState.current_region_stress
+
     vitality_system.tick_environment(delta, env_cold, env_stress)
+    pools.tick_protein(delta, GameState.travel_load, GameState.awake_load)
 
-func tick_protein(delta: float, travel_load: float, awake_load: float) -> void:
-    vitality_system.tick_protein(delta, travel_load, awake_load)
+    if pools.tick_oxygen(delta, GameState.oxygen_drain_rate):
+        GameState.kill_player("OXYGEN_ZERO")
 
-func apply_damage(amount: float) -> void:
-    var dead := vitality_system.apply_damage(amount)
-    if dead:
-        GameState.apply_damage(9999) # force death path
+    var collapsed := pools.tick_stamina(delta, GameState.exertion_level, GameState.stamina_recovery)
+    if collapsed:
+        GameState.on_player_collapse("STAMINA")
+
+    var inf_delta := infection_model.tick_infection(delta, vitality_system, race, GameState.contamination_level)
+    GameState.infection_level += inf_delta
